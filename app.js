8e16435871c8a3fc905f84c7a56240e3edf2dc10
sohaib124 <kamransohaib794@gmail.com>
2025-04-23 10:58:18 +0500

    uodared css and adde index

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-
-let convercurr = document.getElementById('convert-curr')
-let convertcurrency = document.getElementById("covert-currency")
-
-
- async function  covertcurr(){
+let currencydrop = document.getElementById("currencydrop");
+let currencyTo = document.getElementById("currencyTo")
+// let date = document.getElementById("date")
+let from;
+let to;
+ let convertBtn = document.getElementById("convertBtn")
+async function currencyConvert(params) {
+   try{
+    const apiUrl = await fetch("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/country.json");
+    const result = await apiUrl.json()
+     console.log(result);
     
-    let apirl =  await fetch ("https://v6.exchangerate-api.com/v6/fc8851abe3c70b15174a6911/latest/USD")
-    console.log(apirl)
+    for (const key in result) {
+        // console.log(result[key].country_name);
+        currencydrop.innerHTML+=` <option value="${result[key].currency_code}">${result[key].currency_code}-${result[key].country_name}</option>`
+        currencyTo.innerHTML+=`<option value="${result[key].currency_code}">${result[key].currency_code}-${result[key].country_name}</option>`
+
+    }
+
+    // let  date1 = new Date();
     
+    // date.innerHTML=`${date1.toLocaleDateString()}`
+    
+   }catch(err){
+    console.log(err);
+   }
 }
+
+
+
+window.onload = ()=>{
+    currencyConvert();
+}
+
+convertBtn.addEventListener('click', ()=>{
+    let inputValue = document.getElementById("inputValue").value
+    let outputValue = document.getElementById("outputValue").value
+    let fromCurrency = document.getElementById("currencydrop").value
+    let toCurrency = document.getElementById("currencyTo").value})
+    
+    // console.log(code);
+
+    function convertBtn(){
+
+
+    }
+    
+    
+    
